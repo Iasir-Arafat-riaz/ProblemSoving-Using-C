a305365522f0bca2d111dd4e1d502bc282aeffc5
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-04-15 22:41:23 +0600

    new commit

---
+++ HarryLarry.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+int harry,larry;
+
+scanf("%d%d",&harry,&larry);
+harry=harry;
+larry=larry;
+int harryNew=larry-1;
+int larryNew= harry-1;
+printf("%d %d",harryNew, larryNew);
+return 0;
+
+}
---
+++ simpleProblemS.c
@@ -0,0 +1,19 @@
+
+//#include<stdio.h>
+//int main(){
+//
+//int a,b;
+//scanf("%d%d",&a,&b);
+//printf("%d",a+b);
+//
+//return 0;
+//
+//
+//}
+#include<stdio.h>
+int main(){
+
+printf("Hello World!");
+
+return 0 ;
+}
