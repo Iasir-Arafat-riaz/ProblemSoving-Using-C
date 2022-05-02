13fb1cbff043309869d66d91e8a45edbd5d607a7
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-05-02 16:33:24 +0600

    solved new problem

---
+++ c2m9problem.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+/*Write a program that asks the user for a number n and only multiples of three or five are
+considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17*/
+
+int n;
+scanf("%d",&n);
+for(int i=1; i<=n; i++){
+
+
+if(i<0){
+    prin
+}
+    else if(i%3==0||i%5==0){
+        printf("%d\n",i);
+    }
+
+}
+
+
+return 0;
+}
