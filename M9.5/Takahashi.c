7c5b79e07b417ca6879155554c9293eb4ee81de4
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-05-29 22:34:37 +0600

    new problem solved

---
+++ M9.5/Takahashi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+
+
+
+int N,M;
+scanf("%d%d",&N,&M);
+if(N==M){
+   char AC[10]="Yes";
+    printf("%s",AC);
+}
+else if(N!=M){
+   char Ad[10]="No";
+    printf("%s",Ad);
+}
+
+
+return 0;
+}
--- M9.5/bit.c
+++ M9.5/bit.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 int main(){
+//    int lim,bob;
+//    scanf("%d%d",&lim,&bob);
+//   int year=0;
+//
+//
+//
+//for(int i=1;lim <=bob ; i++){
+//        lim*=3;
+//bob*=2;
+//year++;
+//}
+//printf("%d",year);
+int x=0;
 
-
+int n;
+char a,b,c;
+scanf("%d",&n);
+for(int i=0; i<n; i++){
+    scanf(" %c%c%c",&a,&b,&c);
+   if(b=='+'){
+    x+=1;
+   }
+   else if(b=='-'){
+    x-=1;
+   }
+}
+printf("%d\n",x);
 
 return 0;
 }
