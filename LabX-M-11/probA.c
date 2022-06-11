2ae49d130b0c055f9bd343fe52f9e8e7d4bd8e9c
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-06-11 06:52:28 +0600

    hostel room problem solved

--- LabX-M-11/probA.c
+++ LabX-M-11/probA.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 int main(){
 
-int a;
-scanf("%d",&a);
+int room,stdnts, space;
+scanf("%d",&room);
+
+int total = 0;
+for(int i=0; i<room; i++){
+scanf("%d %d",&stdnts,&space);
+if(stdnts<=space && space-stdnts>=2){
+    total+=1;
+}
+
+}
+printf("%d",total);
 
 return 0;
 }
