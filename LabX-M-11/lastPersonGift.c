588ba04b5d38b0b2e5db98ca0e9810bb93fcc944
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-06-19 19:27:43 +0600

    find last person gift problem solved

--- LabX-M-11/lastPersonGift.c
+++ LabX-M-11/lastPersonGift.c
@@ -4,6 +4,24 @@ int main(){
 int N,K,A;
 scanf("%d%d%d",&N,&K,&A);
 
+int lastPerson = A;
+
+if(N>1){
+    for(int i=0; i<K;i++){
+if(lastPerson>N){
+    lastPerson=1;
+}
+
+//printf("%d ",lastPerson);
+lastPerson++;
+
+}
+printf("%d ",lastPerson-1);
+
+}
+if(N==1){
+    printf("%d",N);
+}
 
 
 
